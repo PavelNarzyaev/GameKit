78d8e5986e5851374426b74e51a0f4e392d44359
Pavel Narzyaev <pavel@narzyaev.ru>
2025-01-07 22:23:28 +0400

    Add temporary text to test build

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,9 +1,45 @@
+using TMPro;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+	[SerializeField] private TextMeshProUGUI _helloWorldText;
+
+	private const int _framesForOneStep = 60;
+
+	private AnimationStep _animationStep;
+	private int _framesCounter;
+
+	private enum AnimationStep
+	{
+		Hello,
+		World,
+	}
+
 	private void Start()
 	{
-		Debug.Log("Hello, World!!!");
+		RefreshText();
+	}
+
+	private void Update()
+	{
+		_framesCounter++;
+		if (_framesCounter < _framesForOneStep) return;
+		_framesCounter = 0;
+		_animationStep = _animationStep == AnimationStep.Hello ? AnimationStep.World : AnimationStep.Hello;
+		RefreshText();
+	}
+
+	private void RefreshText()
+	{
+		switch (_animationStep)
+		{
+			case AnimationStep.Hello:
+				_helloWorldText.text = "Hello";
+				break;
+			case AnimationStep.World:
+				_helloWorldText.text = "World!!!";
+				break;
+		}
 	}
 }
