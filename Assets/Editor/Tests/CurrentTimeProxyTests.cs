f0159049d534942675a3207538781eb430ec5477
Pavel Narzyaev <pavel@narzyaev.ru>
2025-05-29 20:31:27 +0400

    Add unit test

---
+++ Assets/Editor/Tests/CurrentTimeProxyTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Zenject;
+
+public class CurrentTimeProxyTests
+{
+	private DiContainer _diContainer;
+
+	[SetUp]
+	public void SetUp()
+	{
+		_diContainer = new DiContainer();
+		_diContainer.Bind<CurrentTimeProxy>().AsSingle();
+	}
+
+	[Test]
+	public void GetTimestamp_WhenCalled_ReturnsPositiveValue()
+	{
+		// Arrange
+		var currentTimeProxy = _diContainer.Resolve<CurrentTimeProxy>();
+
+		// Act
+		var timestamp = currentTimeProxy.GetTimestamp();
+
+		// Assert
+		Assert.That(timestamp, Is.GreaterThan(0));
+	}
+}
