c28e13a6b552e551623cd3f5f727b316a8a95450
Pavel Narzyaev <pavel@narzyaev.ru>
2025-06-03 23:20:35 +0400

    Close shade on click

--- Assets/Scripts/MonoBehaviours/Screens/PopupShadeScreen.cs
+++ Assets/Scripts/MonoBehaviours/Screens/PopupShadeScreen.cs
@@ -1,5 +1,22 @@
-﻿public class PopupShadeScreen : ScreenAbstract
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class PopupShadeScreen : ScreenAbstract
 {
+	[SerializeField] private Button _button;
+	[Inject] private PopupsLayerMediator _popupsLayerMediator;
+
+	private void OnEnable()
+	{
+		_button.onClick.AddListener(_popupsLayerMediator.ClosePopup);
+	}
+
+	private void OnDisable()
+	{
+		_button.onClick.RemoveAllListeners();
+	}
+
 	public override bool IsCached()
 	{
 		return true;
