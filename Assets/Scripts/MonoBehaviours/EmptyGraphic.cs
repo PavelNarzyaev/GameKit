66b328bd5640a309d9ad39724abed56a33491ee2
Pavel Narzyaev <pavel@narzyaev.ru>
2025-06-12 21:38:12 +0400

    Remove summary comment from EmptyGraphic

--- Assets/Scripts/MonoBehaviours/EmptyGraphic.cs
+++ Assets/Scripts/MonoBehaviours/EmptyGraphic.cs
@@ -3,15 +3,6 @@ using UnityEngine.UI;
 
 namespace MonoBehaviours
 {
-    /// <summary>
-    ///     The EmptyGraphic class is an optimized alternative to using a transparent Image component for raycasting purposes.
-    ///     This class inherits from the Unity Graphic class and overrides the OnPopulateMesh method to clear the VertexHelper,
-    ///     effectively rendering nothing.
-    ///     By attaching this script to a GameObject, you can make the object a raycast target without the overhead associated
-    ///     with rendering an actual image. This is particularly useful for empty or invisible objects that still need to
-    ///     respond to user interactions.
-    ///     This class requires a CanvasRenderer component to be attached to the same GameObject.
-    /// </summary>
     [RequireComponent(typeof(CanvasRenderer))]
     public class EmptyGraphic : Graphic
     {
