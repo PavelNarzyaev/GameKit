28e403461f1ed7410e1c9ee2086944ccbc8ce901
Pavel Narzyaev <pavel@narzyaev.ru>
2025-06-13 19:40:09 +0400

    Add buttons to navigation popup screen

--- Assets/Scripts/MonoBehaviours/Screens/NavigationPopupScreen.cs
+++ Assets/Scripts/MonoBehaviours/Screens/NavigationPopupScreen.cs
@@ -1,6 +1,29 @@
+using System;
+using Mediators;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
 namespace MonoBehaviours.Screens
 {
     public class NavigationPopupScreen : ScreenAbstract
     {
+        [SerializeField] private Button mainPageButton;
+        [SerializeField] private Button statePageButton;
+        [SerializeField] private Button timePageButton;
+
+        [Inject] private PagesLayerMediator m_pagesLayerMediator;
+
+        private void Awake()
+        {
+            SetUpButton(mainPageButton, typeof(MainPageScreen));
+            SetUpButton(statePageButton, typeof(StatePageScreen));
+            SetUpButton(timePageButton, typeof(TimePageScreen));
+        }
+
+        private void SetUpButton(Button button, Type pageType)
+        {
+            button.onClick.AddListener(() => m_pagesLayerMediator.ShowPage(pageType));
+        }
     }
 }
