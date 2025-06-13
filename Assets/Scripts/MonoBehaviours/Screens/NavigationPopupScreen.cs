fa1a681e8250ea37b15a98fc2589f07c99b32815
Pavel Narzyaev <pavel@narzyaev.ru>
2025-06-13 19:48:40 +0400

    Add close popup button

--- Assets/Scripts/MonoBehaviours/Screens/NavigationPopupScreen.cs
+++ Assets/Scripts/MonoBehaviours/Screens/NavigationPopupScreen.cs
@@ -11,14 +11,18 @@ namespace MonoBehaviours.Screens
         [SerializeField] private Button mainPageButton;
         [SerializeField] private Button statePageButton;
         [SerializeField] private Button timePageButton;
+        [SerializeField] private Button closeButton;
 
         [Inject] private PagesLayerMediator m_pagesLayerMediator;
+        [Inject] private PopupsLayerMediator m_popupsLayerMediator;
 
         private void Awake()
         {
             SetUpButton(mainPageButton, typeof(MainPageScreen));
             SetUpButton(statePageButton, typeof(StatePageScreen));
             SetUpButton(timePageButton, typeof(TimePageScreen));
+
+            closeButton.onClick.AddListener(m_popupsLayerMediator.CloseNavigationPopup);
         }
 
         private void SetUpButton(Button button, Type pageType)
