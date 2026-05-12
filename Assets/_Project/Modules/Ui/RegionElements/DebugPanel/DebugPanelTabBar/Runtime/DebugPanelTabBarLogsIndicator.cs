16f24552f9ea5c3928cce52c2a09d6d629b16855
Pavel Narzyaev <pavel@narzyaev.ru>
2026-05-12 19:30:10 +0300

    Remove obsolete ExecuteAlways attribute

--- Assets/_Project/Modules/Ui/RegionElements/DebugPanel/DebugPanelTabBar/Runtime/DebugPanelTabBarLogsIndicator.cs
+++ Assets/_Project/Modules/Ui/RegionElements/DebugPanel/DebugPanelTabBar/Runtime/DebugPanelTabBarLogsIndicator.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 namespace GameKit.DebugPanelTabBar
 {
-    [ExecuteAlways]
     public class DebugPanelTabBarLogsIndicator : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         [SerializeField] private GameObject logIndicator;
